6ffaeb8fe4edfb9c62fef8b1f53c22119f71a934
GrigoryCode <grigorycode@gmail.com>
2019-05-10 02:49:24 +0300

    Add categories for products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_09_231723) do
+ActiveRecord::Schema.define(version: 2019_05_09_234620) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -18,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_05_09_231723) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
