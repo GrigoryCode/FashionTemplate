a08acc42331cdd558978c924c84d2eadaae87191
GrigoryCode <grigorycode@gmail.com>
2019-05-10 18:59:45 +0300

    Image size correction

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  has_attached_file :product_img, styles: { product_index: "500x500>", product_show: "800x800>" }, default_url: "/images/:style/missing.png"
+  # has_attached_file :product_img, styles: { product_index: "500x500>", product_show: "800x800>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :product_img, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :product_img, content_type: /\Aimage\/.*\z/
 end
